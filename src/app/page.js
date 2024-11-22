58a617a0233cb7c5c4fe3c0053154f511473bb58
Verere <linno4inno@gmail.com>
2024-11-22 14:34:44 +0100

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -9,15 +9,15 @@ export default function Home() {
     <section className="relative w-full h-full min-h-screen pb-10 bg-blue-200" >
     <div className="w-full h-full relative ">
       <div className="flex  flex-row gap-10 p-3 sm:flex-col items-center">
-        <section className="w-full lg:w-[50%] flex flex-col md:px-2  p-3 lg:p-10 sm:pt-0 sm:mt-0 order-1 sm:order-2">
-          <div className="w-full flex justify-center flex-col h-auto lg:pt-7 sm:pt-0">
+        <section className="w-full lg:w-[50%] sm:w-full flex flex-col md:px-2  p-3 lg:p-10 sm:pt-0 sm:mt-0 order-1 sm:order-2">
+          <div className="w-full sm:tracking-widest flex justify-center flex-col h-auto lg:pt-7 sm:pt-0">
             <span className="flex space-x-2 sm:hidden">
               <span className="block w-14  mb-2 dark:border-white border-b-2 border-gray-700"></span>
               <span className="font-medium w-full dark:text-white text-gray-600">
                 Get the Job done with...
               </span>
             </span>
-            <h1 className="text-6xl dark:text-white mt-5  sm:mt-0 sm:text-4xl lg:text-6xl text-black font-extrabold">
+            <h1 className="text-6xl dark:text-white mt-5 block  sm:mt-0 sm:text-4xl lg:text-6xl w-full text-black font-extrabold">
              Laboratory Information Management System
             </h1>            
           </div>
